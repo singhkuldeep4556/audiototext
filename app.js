d1baf2c434dee3a2a895159f17679f0eb29859cc
singhkuldeep4556 <singhkuldeep4556@gmail.com>
2020-07-10 14:15:42 +0530

    project1

---
+++ app.js
@@ -0,0 +1,33 @@
+class SpeechRecognitionApi {
+    constructor(options) {
+        const SpeechToText = window.SpeechRecognition || window.webkitSpeechRecognition;
+        this.speechApi= new SpeechToText();
+        this.output = options.output ? options.output : document.createElement("div");
+        this.speechApi.continuous = true;
+        this.speechApi.interimResult = false;
+        this.speechApi.onresult=(event)=>{
+            var resultIndex=event.resultIndex;
+            var transcript = event.results[resultIndex][0].transcript;
+            this.output.textContent += transcript;
+        }
+    }
+    init(){
+        this.speechApi.start();
+    }
+    stop(){
+        this.speechApi.stop();
+    }
+   }
+   window.onload = function(){
+       var speech=new SpeechRecognitionApi({
+           output: document.querySelector(".output")
+       })
+   
+       document.querySelector(".btn-start").addEventListener("click",()=>{
+           speech.init();
+       })
+   
+       document.querySelector(".btn-end").addEventListener("click",()=>{
+           speech.stop();
+       })
+   }
